bb24145dc0d6ff5ab28d4133e94f9c1469895aa5
dnowak <dorota.iza.nowak@gmail.com>
2020-05-14 16:01:40 +0200

    example added

--- IFS.py
+++ IFS.py
@@ -13,7 +13,7 @@ class Fractal:
         self.trans = trans
         self.prob = list(itertools.accumulate(prob, lambda x, y: x + y))
 
-    def draw(self, n_iter=50000):
+    def draw(self, n_iter=50000, col='blue'):
         coord = np.zeros((2, n_iter + 1))
         iter = 0
 
@@ -25,12 +25,12 @@ class Fractal:
             iter += 1
             coord[:, iter] = new_coord
 
-        plt.scatter(coord[0, :], coord[1, :], s=0.2, edgecolors='blue')
+        plt.scatter(coord[0, :], coord[1, :], s=0.2, edgecolors=col)
         plt.savefig(f"results/{self.name}")
         plt.show()
 
 
-alpha = 2 * math.pi / 3
+"""alpha = 2 * math.pi / 3
 r = 0.7
 f1 = np.array([[r * cos(alpha), -r * sin(alpha), 0], [r * sin(alpha), r * cos(alpha), 1]])
 f2 = np.array([[r * cos(alpha), r * sin(alpha), 0], [-r * sin(alpha), r * cos(alpha), 1]])
@@ -39,4 +39,13 @@ transformations = [f1, f2, f3]
 probabilities = [33, 33, 34]
 
 symmetric_tree = Fractal('symmetric-tree', transformations, probabilities)
-symmetric_tree.draw()
+symmetric_tree.draw()"""
+
+g1 = np.array([[0,0,0],[0,0.16,0]])
+g2 = np.array([[0.85, 0.04, 0],[-0.04, 0.85, 1.6]])
+g3 = np.array([[0.2, -0.26, 0],[0.23, 0.22, 1.6]])
+g4 = np.array([[-0.15, 0.28, 0],[0.26, 0.24, 0.44]])
+t = [g1,g2,g3,g4]
+p = [1,85,7,7]
+barnsley_fern = Fractal('barnsley_fern', t, p)
+barnsley_fern.draw(col='green')
